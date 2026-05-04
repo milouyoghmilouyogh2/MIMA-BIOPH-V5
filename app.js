d4cced78ee72714189b2a6e44b7d18dd442cc083
milouyoghmilouyogh2 <milouyoghmilouyogh2@gmail.com>
2026-05-04 17:11:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ═══════════════════════════════════════════
 // app.js — Application Core v3.0
-//  Dr El Hakima · 1ère Année Médecine
+// Dr El Hakima · 1ère Année Médecine
 // ═══════════════════════════════════════════
 'use strict';
 
